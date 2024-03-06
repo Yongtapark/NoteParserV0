aad5a5104a28e13d5ba6b11a29c94a250ca8d68d
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-06 13:11:41 +0900

    add : 예외추가

--- src/test/java/com/example/demo/NoteParser.java
+++ src/test/java/com/example/demo/NoteParser.java
@@ -3,25 +3,36 @@ package com.example.demo;
 import static com.example.demo.enums.NoteRegex.NOTE_FORM;
 
 import com.example.demo.enums.NoteRegex;
+import com.example.demo.exceptions.NoteFormatException;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 
 public class NoteParser {
-    public HashMap<NoteRegex,HashMap<String,String>> extractAndSaveNote(HashMap<NoteRegex,HashMap<String,String>> tags, String content){
+    public HashMap<NoteRegex, HashMap<String, String>> extractAndSaveNote(
+            HashMap<NoteRegex, HashMap<String, String>> tags, String content) {
         Matcher extractIdAndNote = NOTE_FORM.extractString(content);
-        if(extractIdAndNote.matches()){
+        if (extractIdAndNote.matches()) {
             String ids = extractIdAndNote.group(1);
             String note = extractIdAndNote.group(2);
-            for(NoteRegex regex : NoteRegex.values()){
+            String[] idArray = ids.split(",");
+
+            for (NoteRegex regex : NoteRegex.values()) {
                 Matcher regexMatcher = regex.getCompile().matcher(ids);
-                if(regexMatcher.matches()){
-                    String[] idArray = ids.split(",");
-                    HashMap<String, String> idAndNoteMap = tags.computeIfAbsent(regex,k->new HashMap<>());
-                    for (String id : idArray) {
-                        idAndNoteMap.put(id,note);
+                if (regexMatcher.matches()) {
+                    HashMap<String, String> idAndNoteMap = tags.computeIfAbsent(regex, k -> new HashMap<>());
+                    for (int i = 0; i < idArray.length; i++) {
+                        idAndNoteMap.put(idArray[i], note);
+                        idArray[i] = null;
                     }
                 }
             }
+
+            for (int i = 0; i < idArray.length; i++) {
+                if (idArray[i] != null) {
+                    throw new NoteFormatException(content);
+                }
+            }
+
         }
         return tags;
     }
