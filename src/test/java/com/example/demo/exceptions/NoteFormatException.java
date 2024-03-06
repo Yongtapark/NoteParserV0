205035640ad1600183b3276baeb87b61ff8d3899
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-06 12:35:44 +0900

    add : 예외추가

---
+++ src/test/java/com/example/demo/exceptions/NoteFormatException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exceptions;
+
+public class NoteFormatException extends AnnounceErrorException {
+    private final static String DEFAULT_MESSAGE = "유효하지 않은 노트입니다. 다시 입력해 주세요 = ";
+
+    public NoteFormatException(String message) {
+        super(DEFAULT_MESSAGE + message);
+    }
+}
