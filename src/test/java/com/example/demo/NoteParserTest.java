1d252c26900de1c49e3ba6879451c3748cd32787
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-06 15:45:36 +0900

    test : 테스트 상황 추가

--- src/test/java/com/example/demo/NoteParserTest.java
+++ src/test/java/com/example/demo/NoteParserTest.java
@@ -12,8 +12,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 class NoteParserTest {
     @Test
     @DisplayName("특정 문자열을 추출해 regex 별로 id, note 로 구분하여 해시맵에 저장한다.")
-    void test1() {
+    void test1_BARN() {
         final String ID = "1번축사";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        tags = noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE);
+
+        String note = "NOTHING";
+        for (HashMap<String, String> hashMap : tags.values()) {
+            if (hashMap.containsKey(ID)) {
+                note = hashMap.get(ID);
+            }
+        }
+        Assertions.assertThat(note).isEqualTo(NOTE);
+    }
+    @Test
+    @DisplayName("특정 문자열을 추출해 regex 별로 id, note 로 구분하여 해시맵에 저장한다.")
+    void test1_PEN() {
+        final String ID = "10-15";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        tags = noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE);
+
+        String note = "NOTHING";
+        for (HashMap<String, String> hashMap : tags.values()) {
+            if (hashMap.containsKey(ID)) {
+                note = hashMap.get(ID);
+            }
+        }
+        Assertions.assertThat(note).isEqualTo(NOTE);
+    }
+    @Test
+    @DisplayName("특정 문자열을 추출해 regex 별로 id, note 로 구분하여 해시맵에 저장한다.")
+    void test1_COW() {
+        final String ID = "1015";
         final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
         NoteParser noteParser = new NoteParser();
         HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
@@ -30,8 +64,38 @@ class NoteParserTest {
 
     @Test
     @DisplayName("잘못된 형식으로 입력 시 예외를 발생시킨다")
-    void name2() {
+    void test2_justComma() {
         final String ID = "1번축사,";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        Assertions.assertThatThrownBy(() -> noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE))
+                .isInstanceOf(NoteFormatException.class);
+    }
+    @Test
+    @DisplayName("잘못된 형식으로 입력 시 예외를 발생시킨다")
+    void test2_specialCharacter() {
+        final String ID = "1번축사@";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        Assertions.assertThatThrownBy(() -> noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE))
+                .isInstanceOf(NoteFormatException.class);
+    }
+    @Test
+    @DisplayName("공백 입력 시 예외를 발생시킨다")
+    void test2_spaceBar() {
+        final String ID = " ";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        Assertions.assertThatThrownBy(() -> noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE))
+                .isInstanceOf(NoteFormatException.class);
+    }
+    @Test
+    @DisplayName("공백 입력 시 예외를 발생시킨다")
+    void test2_nothing() {
+        final String ID = "";
         final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
         NoteParser noteParser = new NoteParser();
         HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
@@ -41,7 +105,7 @@ class NoteParserTest {
 
     @Test
     @DisplayName("복수로 id값을 입력하면 두 id에 동일한 note 가 저장된다.")
-    void name3() {
+    void test3() {
         final String IDS = "1번축사,2번축사";
         final String ID1 = "1번축사";
         final String ID2 = "2번축사";
