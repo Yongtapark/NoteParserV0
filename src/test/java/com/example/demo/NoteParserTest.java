ca37d945de20df84e97852ece45e2a61e22b00b8
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-06 12:32:41 +0900

    add : 테스트 케이스 추가

--- src/test/java/com/example/demo/NoteParserTest.java
+++ src/test/java/com/example/demo/NoteParserTest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.enums.NoteRegex;
+import com.example.demo.exceptions.NoteFormatException;
 import java.util.HashMap;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,13 +17,46 @@ class NoteParserTest {
         final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
         NoteParser noteParser = new NoteParser();
         HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
-        tags = noteParser.extractAndSaveNote(tags, "[["+ID+"]] "+NOTE);
+        tags = noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE);
+
         String note = "NOTHING";
         for (HashMap<String, String> hashMap : tags.values()) {
-            if(hashMap.containsKey(ID)) {
+            if (hashMap.containsKey(ID)) {
                 note = hashMap.get(ID);
             }
         }
         Assertions.assertThat(note).isEqualTo(NOTE);
+    }
+
+    @Test
+    @DisplayName("잘못된 형식으로 입력 시 예외를 발생시킨다")
+    void name2() {
+        final String ID = "1번축사,";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        Assertions.assertThatThrownBy(() -> noteParser.extractAndSaveNote(tags, "[[" + ID + "]] " + NOTE))
+                .isInstanceOf(NoteFormatException.class);
+    }
+
+    @Test
+    @DisplayName("복수로 id값을 입력하면 두 id에 동일한 note 가 저장된다.")
+    void name3() {
+        final String IDS = "1번축사,2번축사";
+        final String ID1 = "1번축사";
+        final String ID2 = "2번축사";
+        final String NOTE = "오늘 저녁도 카레 샐러드다. 내일은 뭘 먹지";
+        NoteParser noteParser = new NoteParser();
+        HashMap<NoteRegex, HashMap<String, String>> tags = new HashMap<>();
+        tags = noteParser.extractAndSaveNote(tags, "[[" + IDS + "]] " + NOTE);
+
+        String note1 = "NOTHING";
+        String note2 = "NOTHING";
+        for (HashMap<String, String> hashMap : tags.values()) {
+            note1 = hashMap.get(ID1);
+            note2 = hashMap.get(ID2);
+        }
+        Assertions.assertThat(note1).isEqualTo(NOTE);
+        Assertions.assertThat(note2).isEqualTo(NOTE);
     }
 }
