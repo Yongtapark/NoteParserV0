f5556ef1faa897aa46bac2f180a3e83d06e0604b
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-09 15:34:29 +0900

    refactor : 사용자 정의 객체로 반환형 변경

--- src/main/java/com/example/demo/NoteParserController.java
+++ src/main/java/com/example/demo/NoteParserController.java
@@ -1,14 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.domain.enums.NoteRegex;
-import java.util.Map;
+import com.example.demo.domain.note_parser.NoteContainer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api/note-parser/")
@@ -22,8 +19,8 @@ public class NoteParserController {
 
 
     @PostMapping("/parse")
-    public ResponseEntity<Map<NoteRegex, Map<String, String>> > parseNote(@RequestBody String content) {
-        Map<NoteRegex, Map<String, String>> tags = noteParserService.saveContentUseNoteParser(content);
-        return ResponseEntity.ok(tags);
+    public ResponseEntity<NoteContainer> parseNote(@RequestBody String content) {
+        NoteContainer noteContainer = noteParserService.saveContentUseNoteParser(content);
+        return ResponseEntity.ok(noteContainer);
     }
 }
