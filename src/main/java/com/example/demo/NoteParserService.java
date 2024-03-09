c4b5d18ddfdb10828cdb324fc36e68fa8f7ed087
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-09 15:34:20 +0900

    refactor : 사용자 정의 객체로 반환형 변경

--- src/main/java/com/example/demo/NoteParserService.java
+++ src/main/java/com/example/demo/NoteParserService.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.domain.NoteParser;
-import com.example.demo.domain.enums.NoteRegex;
-import java.util.HashMap;
-import java.util.Map;
+import com.example.demo.domain.note_parser.NoteContainer;
+import com.example.demo.domain.note_parser.NoteParser;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,8 +13,8 @@ public class NoteParserService {
         this.noteParser = noteParser;
     }
 
-    public Map<NoteRegex, Map<String, String>> saveContentUseNoteParser(String content) {
-        Map<NoteRegex, Map<String, String>> tags = new HashMap<>();
-        return noteParser.extractAndSaveNote(tags, content);
+    public NoteContainer saveContentUseNoteParser(String content) {
+        NoteContainer noteContainer = new NoteContainer();
+        return noteParser.extractAndSaveNote(noteContainer, content);
     }
 }
