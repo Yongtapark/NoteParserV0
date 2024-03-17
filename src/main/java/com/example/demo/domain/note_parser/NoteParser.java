185747fd96dee890474b028dd23b9f5319baed0b
Yong Ta Park <qkrdydxk80@nate.com>
2024-03-17 13:32:18 +0900

    fix : 각 note 별로 list.add()하여 저장

--- src/main/java/com/example/demo/domain/note_parser/NoteParser.java
+++ src/main/java/com/example/demo/domain/note_parser/NoteParser.java
@@ -3,46 +3,50 @@ package com.example.demo.domain.note_parser;
 
 import com.example.demo.domain.enums.NoteRegex;
 import com.example.demo.domain.exceptions.NoteFormatException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NoteParser {
-    final static int GROUP_ONE = 1;
-    final static int GROUP_TWO = 2;
-    final static String SPLIT_DELIMITER = ",";
-    public Map<NoteRegex, Map<String, String>> extractAndSaveNote(
-            Map<NoteRegex, Map<String, String>> tags, String content) {
+    final static int ID_GROUP_INDEX = 1;
+    final static int NOTE_GROUP_INDEX = 2;
+    final static String ID_DELIMITER = ",";
+
+    public Map<NoteRegex, List<IdAndNote>> extractAndSaveNote(
+            Map<NoteRegex, List<IdAndNote>> tags, String content) {
         String[] split = content.split(System.lineSeparator());
         for (String eachContext : split) {
             Matcher extractIdAndNote = NoteRegex.getNoteFormMatcher(eachContext);
-            if (extractIdAndNote.matches()) {
-                String[] idArray = extractAndSaveToMap(tags, extractIdAndNote);
-                checkUnMatched(content, idArray);
-            } else {
+            if (!extractIdAndNote.matches()) {
                 throw new NoteFormatException(content);
             }
+            String[] idArray = extractAndSaveToMap(tags, extractIdAndNote);
+            checkUnMatched(eachContext, idArray);
+
         }
 
         return tags;
     }
 
-    private static String[] extractAndSaveToMap(Map<NoteRegex, Map<String, String>> tags, Matcher extractIdAndNote) {
-        String ids = extractIdAndNote.group(GROUP_ONE);
-        String note = extractIdAndNote.group(GROUP_TWO);
-        String[] idArray = ids.split(SPLIT_DELIMITER);
+    private static String[] extractAndSaveToMap(Map<NoteRegex, List<IdAndNote>> tags, Matcher extractIdAndNote) {
+        String ids = extractIdAndNote.group(ID_GROUP_INDEX);
+        String note = extractIdAndNote.group(NOTE_GROUP_INDEX);
+        String[] idArray = ids.split(ID_DELIMITER);
 
         for (NoteRegex regex : NoteRegex.values()) {
             Matcher regexMatcher = regex.getCompile().matcher(ids);
             if (regexMatcher.matches()) {
-                Map<String, String> idAndNoteMap = tags.computeIfAbsent(regex, k -> new HashMap<>());
+                List<IdAndNote> idAndNotesList = tags.computeIfAbsent(regex,k->new ArrayList<>());
+
                 for (int i = 0; i < idArray.length; i++) {
-                    idAndNoteMap.put(idArray[i], note);
+                    IdAndNote idAndNote = new IdAndNote(idArray[i], note);
+                    idAndNotesList.add(idAndNote);
                     idArray[i] = null;
                 }
-                tags.put(regex, idAndNoteMap);
+                tags.put(regex, idAndNotesList);
             }
         }
         return idArray;
